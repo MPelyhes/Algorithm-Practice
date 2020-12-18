7839a7d7c6b23c707b2126e11532f872b03ca80e
Matt Pelyhes <mattpelyhes@gmail.com>
2020-12-18 15:43:41 -0600

    diff_array_algo

--- algo.js
+++ algo.js
@@ -17,4 +17,16 @@ function sumAll(arr) {
   return result;
 }
 
-console.log(sumAll([1,4]));
+// console.log(sumAll([1,4]));
+
+// Diff Two Arrays
+
+/* Compare two arrays and return a new array with any items found in one of the two 
+given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+*/
+
+function diffArray(arr1, arr2) {
+  return arr1.concat(arr2).filter((item,_,arr) => arr.lastIndexOf(item) == arr.indexOf(item));
+}
+
+// console.log(diffArray([1,2,3,4], [2,4,6,8]));
