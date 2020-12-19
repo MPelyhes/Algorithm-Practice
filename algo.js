ef1fe8fb986fd107de5985e72f6591546e1835d7
Matt Pelyhes <mattpelyhes@gmail.com>
2020-12-19 09:55:08 -0600

    edited whatIsInAName algo

--- algo.js
+++ algo.js
@@ -53,21 +53,30 @@ function destroyer(arr) {
 
 // Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
 
-// function whatIsInAName(collection, source) {
-//   var arr = [];
-//   // Only change code below this line
-//   // for(let i = 0; i < collection.length; i++) {
-//   //   if(collection[i].hasOwnProperty(source)) {
-//   //     arr.push(collection[i])
-//   //   }
-//   // }
-//   var srcKeys = Object.keys(source);
-//   var srcVals = Object.values(source);
-//   // Only change code above this line
-//   return srcKeys.concat(srcVals);
-// }
+function whatIsInAName(collection, source) {
+  // We create a variable for the source objects keys
+  let srcKeys = Object.keys(source);
 
-// console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+  // filter the collection
+  return collection.filter((obj) => {
+    for (let i = 0; i < srcKeys.length; i++) {
+      if (!obj.hasOwnProperty(srcKeys[i]) ||
+      obj[srcKeys[i]] !== source[srcKeys[i]]
+      ) {
+        return false;
+      }
+    }
+    return true;
+  })
+}
+
+  /* We filter through the collection array, and use a for loop to loop
+  through each object in the array. We use an if statement to check if
+  the object in the collection doesn't have the key and the property
+  value doesn't match the value in source. We return false if the if
+  statement is correct, otherwise, we return true.*/
+
+console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
 
 // Spinal Tap Case  
 
@@ -81,5 +90,5 @@ function spinalCase(str) {
  
 }
 
-console.log(spinalCase("This is a sentence to test spinal case"));
+// console.log(spinalCase("This is a sentence to test spinal case"));
 
