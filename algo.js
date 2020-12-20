bc01aa6aa651c2e0a667f4e7958cb5da27834cfe
Matt Pelyhes <mattpelyhes@gmail.com>
2020-12-20 15:52:10 -0600

    add DNA pairing algo

--- algo.js
+++ algo.js
@@ -159,6 +159,43 @@ capital letter. If it does, we use the replaceRegex to search
 the str and replace it with upperAfter. otherwise we replace it
 with lowerAfter
 */
-console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"));
+// console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"));
 
+// DNA Pairing
 
+/*The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+Return the provided character as the first element in each array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+*/
+
+function pairElement(str) {
+  let strArray = str.split('');
+  let arr = [];
+
+  for (let i = 0; i < strArray.length; i++ ){
+    switch(strArray[i]) {
+      case "A":
+        arr.push(["A", "T"]);
+        break;
+      case "T":
+        arr.push(["T", "A"]);
+        break;
+      case "G":
+        arr.push(["G", "C"]);
+        break;
+      case "C":
+        arr.push(["C", "G"]);
+        break
+    }
+    // return arr;
+  }
+  return arr;
+}
+
+console.log(pairElement("ATCGA"));
