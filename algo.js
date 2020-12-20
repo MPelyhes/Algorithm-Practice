0644edf8a5c71a9577880048e4497706036d8850
Matt Pelyhes <mattpelyhes@gmail.com>
2020-12-20 15:12:45 -0600

    add search and replace algo

--- algo.js
+++ algo.js
@@ -121,4 +121,44 @@ function translatePigLatin(str) {
  vowel, we return the string and concat "way". Else, we take the
  vowelEnd characters and concat conStart and "ay". */
 
-console.log(translatePigLatin("mythical"));
+// console.log(translatePigLatin("mythical"));
+
+// Search and Replace
+
+/* Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+-First argument is the sentence to perform the search and replace on.
+-Second argument is the word that you will be replacing (before).
+-Third argument is what you will be replacing the second argument with (after).
+-Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
+*/
+
+function myReplace(str, before, after) {
+  
+  const replaceRegex = new RegExp(`${before}`);
+  const caseRegex = /^[A-Z]/;
+  let upperAfter = after.charAt(0).toUpperCase() + after.slice(1);
+  let lowerAfter = after.charAt(0).toLowerCase() + after.slice(1)
+
+  if(caseRegex.test(before)) {
+    return str.replace(replaceRegex, upperAfter);
+  } else {
+    after[0].toLowerCase();
+    return str.replace(replaceRegex, lowerAfter);
+  }
+}
+
+/*We start by creating two regular expressions. The first one is created
+with a RegExp() constructor, so that the before argument can be
+dynamically added. The secod regex checks for if a string starts with
+an uppercase letter. We create two vars (upperAfter, lowerAfter) to
+mutate the after argument. One changes the first character to
+uppercase, the other to lowercase. We use slice() to grab every
+character after the first one and concat it to the firs character.
+Then we use an if statement to test if before starts with a
+capital letter. If it does, we use the replaceRegex to search
+the str and replace it with upperAfter. otherwise we replace it
+with lowerAfter
+*/
+console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"));
+
+
