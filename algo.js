78739e03a7cd4fac2653392129ae13b3cb7bda0e
Matt Pelyhes <mattpelyhes@gmail.com>
2020-12-22 17:16:26 -0600

    add fibonaci sum algo

--- algo.js
+++ algo.js
@@ -279,4 +279,35 @@ Replace the HTML entities with their corresponding HTML entity string (i.e. '&' 
 temp.join('') converts the array of characters into a string to be returned.
 */
 
-console.log(convertHTML("Sixty > twelve"));
+// console.log(convertHTML("Sixty > twelve"));
+
+// Sum All Odd Fibonacci Numbers
+
+/* Given a positive integer num, return the sum of all odd Fibonacci
+numbers that are less than or equal to num.*/
+
+function sumFibs(num) {
+  let prevNumber = 0;
+  let currNumber = 1;
+  let result = 0;
+
+  while (currNumber <= num) {
+    if (currNumber % 2 !==0) {
+      result += currNumber;
+    }
+
+    currNumber +=prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
+}
+
+console.log(18);
+
+/*We create a variable to keep record of the current and previous numbers along with the result that will be returned.
+We use a while loop to make sure we do not go over the number given as parameter.
+We use the modulo operand to check if the current number is odd or even. If it is odd, add it to the result.
+We complete the Fibonacci circle by rotating getting the next number and swapping values after.
+Return the result.
+*/
