c00068b225b69b988981af2c9550f97ab481e7a2
Matt Pelyhes <mattpelyhes@gmail.com>
2020-12-28 15:46:00 -0600

    Add make a person algo

--- algo.js
+++ algo.js
@@ -482,4 +482,51 @@ the second argument is a number. If they both are, we return the sum of
 the first and second arguments. Else ff there is only one argument, we 
 reutrn a function.*/
 
-console.log(addTogether(2,3));
+// console.log(addTogether(2,3));
+
+// Make a Person
+
+/*Fill in the object constructor with the following methods below:
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+Run the tests to see the expected output for each method. The methods
+that take an argument must accept only one argument and it has to be a
+string. These methods must be the only available means of interacting 
+with the object. */
+
+var Person = function(firstAndLast) {
+  // Only change code below this line
+  // Complete the method below and implement the others similarly
+  var fullName = firstAndLast;
+
+  this.getFirstName = function() {
+    return fullName.split(" ")[0];
+  };
+
+  this.getLastName = function() {
+    return fullName.split(" ")[1];
+  };
+
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  this.setFirstName = function(name) {
+    fullName = name + " " + fullName.split(" ")[1];
+  };
+
+  this.setLastName = function(name) {
+    fullName = fullName.split(" ")[0] + " " + name;
+  };
+
+  this.setFullName = function(name) {
+    fullName = name;
+  };
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
