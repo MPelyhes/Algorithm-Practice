9da91fd0a010fff8eb162472cbd715ca7988a69d
Matt Pelyhes <mattpelyhes@gmail.com>
2020-12-28 16:42:50 -0600

    Add  Stop gninnipS My sdroW! algo

--- algo.js
+++ algo.js
@@ -528,5 +528,34 @@ var Person = function(firstAndLast) {
   };
 };
 
-var bob = new Person('Bob Ross');
-bob.getFullName();
+// var bob = new Person('Bob Ross');
+// bob.getFullName();
+
+
+//Stop gninnipS My sdroW!
+
+/*Write a function that takes in a string of one or more words, and
+returns the same string, but with all five or more letter words reversed 
+(Just like the name of this Kata). Strings passed in will consist of 
+only letters and spaces. Spaces will be included only when more than one 
+word is present.
+Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef
+sroirraw" spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test" */
+
+function spinWords(str){
+  let arrWords = str.split(' ');
+  let result = [];
+  
+  for(let i = 0;  i < arrWords.length;  i++){
+    if(arrWords[i].length >= 5) {
+      let reversed =arrWords[i].split('').reverse().join('');
+      result.push(reversed)
+    } else {
+      result.push(arrWords[i])
+    }
+ }
+return result.join(' ');
+}
+
+console.log(spinWords('Hey fellow warriors'));
