7d001b2b518b2e162b252f0dc50a4bf7fbf22cd6
Matt Pelyhes <mattpelyhes@gmail.com>
2021-01-12 19:31:56 -0600

    Add array diff algo

--- algo.js
+++ algo.js
@@ -875,3 +875,27 @@ function bouncingBall(h,  bounce,  window) {
 
   return counter;
 }
+
+// Array Diff
+
+/*Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+It should remove all values from list a, which are present in list b.
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+arrayDiff([1,2,2,2,3],[2]) == [1,3] */
+
+function arrayDiff(a, b) {
+  let result = [];
+
+  for(let i = 0; i < a.length; i++){
+      if(!b.includes(a[i])){
+        result.push(a[i])
+      } 
+  }
+
+  return result;
+}
+
+console.log(arrayDiff([], [4,5]));
+console.log(arrayDiff([3,4], [3]));
+console.log(arrayDiff([1,8,2], []));
