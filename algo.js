7fb7afbc6323ba3802482a11d475235183021bc5
Matt Pelyhes <mattpelyhes@gmail.com>
2021-02-19 16:07:14 -0600

    add snail sort algo

--- algo.js
+++ algo.js
@@ -1089,3 +1089,34 @@ function perimeter(n) {
 }
 
 // console.log(perimeter(7));
+
+// Snail Sort
+// Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+// array = [[1,2,3],
+//          [4,5,6],
+//          [7,8,9]]
+// snail(array) #=> [1,2,3,6,9,8,7,4,5]
+// For better understanding, please follow the numbers of the next array consecutively:
+
+// array = [[1,2,3],
+//          [8,9,4],
+//          [7,6,5]]
+// snail(array) #=> [1,2,3,4,5,6,7,8,9]
+
+snail = function(array) {
+  const storeArr = [];
+  while(array.length){
+    storeArr.push(...array.shift())
+    for(let i=0; i < array.length; i++){
+      storeArr.push(array[i].pop())
+    }
+    storeArr.push(...(array.pop() || []).reverse())
+    for(let i=array.length-1; i>=0; i--){
+      storeArr.push(array[i].shift())
+    }
+  }
+ return storeArr;
+}
+
+console.log(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
