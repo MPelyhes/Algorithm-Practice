9e08ed873490112d2963a646d6a0b90edc83e073
MPelyhes <mattpelyhes@gmail.com>
2021-03-18 21:31:39 -0500

    add leapyear algo solution

--- algoLadder.js
+++ algoLadder.js
@@ -315,4 +315,27 @@ const fizzBuzz = (num) => {
   return result;
 }
 
-console.log(fizzBuzz(100));
+// console.log(fizzBuzz(100));
+
+//Algo 17: Leap Year
+// Given a year, report if it is a leap year.
+// The tricky thing here is that a leap year in the Gregorian calendar occurs:
+// on every year that is evenly divisible by 4
+// except every year that is evenly divisible by 100
+// unless the year is also evenly divisible by 400
+// For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+
+const leapYear = (num) => {
+  if(Number.isInteger(num/100) && !Number.isInteger(num/400)){
+    return false;
+  } else if(Number.isInteger(num/4)){
+    return true;
+  }
+  
+  return false;
+}
+
+// console.log(leapYear(200));
+
+//For this solution we start by checking for the one instance in which a number divisible by four is not a leap year
+//Next we check if the number is divisible by four. If it is, then we have ourselves a leap year! Otherwise, we return false.
