8dd82668308181e43069abb4eb44ae3a22245fb2
Matt Pelyhes <mattpelyhes@gmail.com>
2021-05-04 08:32:16 -0500

    optimize bubble sort

--- aprilalgo.js
+++ aprilalgo.js
@@ -681,15 +681,19 @@ const stringSearch = (str1, str2) => {
 
 const bubbleSort = (arr) => {
   for(let i = arr.length; i > 0; i--){
+    let noSwap = true;
     for(let j = 0; j < i -1; j++){
       if(arr[j] > arr[j + 1]){
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwap = false;
       }
     }
+    if(noSwap) break;
   }
   return arr;
 }
 
 // console.log(bubbleSort([1,4,3,6,2,7,78,62,5,10,12]));
+// console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]))
