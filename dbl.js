96e670b1895ea88be5e70cec894bdfc65656bde0
Matt Pelyhes <mattpelyhes@gmail.com>
2021-05-11 08:30:09 -0500

    add push method to dbl class

--- dbl.js
+++ dbl.js
@@ -14,4 +14,24 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val){
+    let newNode = new Node(val);
+    if(this.head === null){
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+let list = new DoublyLinkedList();
+
+list.push("Hi, again.");
+list.push("Is it me you're looking for?");
+list.push("Well, here I am!");
+console.log(list.head.next.prev);
