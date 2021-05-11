b155794dc89c85622821eeed85e187050931d8d5
Matt Pelyhes <mattpelyhes@gmail.com>
2021-05-11 15:34:14 -0500

    add pop method

--- dbl.js
+++ dbl.js
@@ -27,6 +27,20 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  pop(){
+    if(!this.head) return undefined;
+    let oldTail = this.tail;
+    if(this.length === 1) {
+      this.head =null;
+      this.tail = null;
+    } else {
+      this.tail = oldTail.prev;
+      this.tail.next = null;
+      oldTail.prev = null;
+    }
+    this.length--;
+    return oldTail;
+  }
 }
 
 let list = new DoublyLinkedList();
@@ -34,4 +48,5 @@ let list = new DoublyLinkedList();
 list.push("Hi, again.");
 list.push("Is it me you're looking for?");
 list.push("Well, here I am!");
-console.log(list.head.next.prev);
+console.log(list.pop());
+console.log(list.tail);
