950e294886f241579fe4bf6a2c8461634092b353
Matt Pelyhes <mattpelyhes@gmail.com>
2021-05-19 14:53:07 -0500

    add addedge method

--- graph.js
+++ graph.js
@@ -5,10 +5,15 @@ class Graph {
   addVertex(name){
     this.adjacencyList[name] = [];
   }
+  addEdge(vertex1, vertex2){
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
 }
 
 const graph = new Graph();
 graph.addVertex("Hello");
 graph.addVertex("Goodbye");
 graph.addVertex("HastaLaVista");
+graph.addEdge("Hello", "Goodbye");
 console.log(graph)
