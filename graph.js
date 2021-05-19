b6c2a8d4190f5cb34d37647e038395ff0e818d86
Matt Pelyhes <mattpelyhes@gmail.com>
2021-05-19 15:22:17 -0500

    add removeedge and removevertex methods

--- graph.js
+++ graph.js
@@ -9,6 +9,16 @@ class Graph {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
+  removeEdge(vertex1, vertex2){
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(value => value !== vertex2);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(value => value !== vertex1 )
+  }
+  removeVertex(vertex){
+    while(this.adjacencyList[vertex].length > 0){
+      this.removeEdge(vertex, this.adjacencyList[vertex].pop());
+    }
+    delete this.adjacencyList[vertex];
+  }
 }
 
 const graph = new Graph();
@@ -16,4 +26,7 @@ graph.addVertex("Hello");
 graph.addVertex("Goodbye");
 graph.addVertex("HastaLaVista");
 graph.addEdge("Hello", "Goodbye");
+graph.addEdge("Hello", "HastaLaVista");
+console.log(graph)
+graph.removeVertex("Hello");
 console.log(graph)
