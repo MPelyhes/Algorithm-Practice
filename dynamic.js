eee9db25e4d158bc6cbe6d5560af8656876bbcb0
Matt Pelyhes <mattpelyhes@gmail.com>
2021-05-21 14:50:58 -0500

    add recursive memoization fibonacci function

--- dynamic.js
+++ dynamic.js
@@ -19,11 +19,22 @@ const fibonacci = (num) => {
   return sum;
 }
 
-//Shorter recursive fibonacci
+//Shorter recursive fibonacci (Not as efficient, though)
 const fibShort = (num)  => {
   if(num < 2) return 1;
   return fibShort(num -1) + fibShort(num-2);
 }
 
-console.log(fibShort(5))
-console.log(fibonacci(5))
+//recursive fibonacci function that takes advantage of memoization
+const fibMemo = (n, memo=[]) => {
+  console.log("here")
+  if(memo[n] !== undefined) return memo[n];
+  if(n <= 2) return 1;
+  let res = fibMemo(n-1, memo) + fibMemo(n-2, memo);
+  memo[n] = res;
+  return res;
+}
+
+console.log(fibShort(10))
+console.log(fibonacci(10))
+console.log(fibMemo(10));
