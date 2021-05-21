d49645a3b81f4748e606407c3296790447dc183c
Matt Pelyhes <mattpelyhes@gmail.com>
2021-05-20 19:16:35 -0500

    add dijkstra's algorithm solution

--- weighted.js
+++ weighted.js
@@ -9,6 +9,58 @@ class WeightedGraph {
     this.adjacencyList[vertex1].push({node:vertex2, weight});
     this.adjacencyList[vertex2].push({node:vertex1, weight});
   }
+  Dijkstra(start, finish){
+    const distances = {};
+    const nodes = new PriorityQueue();
+    const previous = {};
+    let path = []; // to return at the end
+    let smallest;
+
+    // Build up initial state
+    for(let vertex in this.adjacencyList){
+      if(vertex === start){
+        distances[vertex] = 0;
+        nodes.enqueue(vertex, 0);
+      } else {
+        distances[vertex] = Infinity;
+        nodes.enqueue(vertex, Infinity);
+      }
+      previous[vertex] = null;
+    }
+    // run as long as there is something in the queue
+    while(nodes.values.length){
+      smallest = nodes.dequeue().val;
+      if(smallest === finish){
+        //We are done! Build up path to return at the end
+        while(previous[smallest]){
+          path.push(smallest);
+          smallest = previous[smallest];
+        }
+        break;
+      }
+      if(smallest || distances[smallest] !== Infinity){
+        for(let neighbor in this.adjacencyList[smallest]){
+          //Find neighboring node
+          let nextNode = this.adjacencyList[smallest][neighbor];
+          console.log(nextNode);
+          //calculate new distances to neighboring node
+          let candidate = distances[smallest] + nextNode.weight;
+          let nextNeighbor = nextNode.node;
+          if(candidate < distances[nextNeighbor]){
+            //updating new smallest distance to neighbor
+            distances[nextNeighbor] = candidate;
+            //updating previous - How we got to neighbor
+            previous[nextNeighbor] = smallest;
+            //enqueue in priority queue with new priority
+            nodes.enqueue(nextNeighbor, candidate);
+          }
+        }
+      }
+    }
+    console.log(distances)
+    console.log(previous);
+    return path.concat(smallest).reverse();
+  }
 }
 
 class PriorityQueue{
@@ -32,8 +84,17 @@ let graph = new WeightedGraph();
 graph.addVertex("A");
 graph.addVertex("B");
 graph.addVertex("C");
-graph.addEdge("A", "B", 9);
-graph.addEdge("A", "C", 5);
-graph.addEdge("B", "C", 7);
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
 
-console.log(graph.adjacencyList);
+graph.addEdge("A", "B", 4);
+graph.addEdge("A", "C", 2);
+graph.addEdge("B", "E", 3);
+graph.addEdge("C", "D", 2);
+graph.addEdge("C", "F", 4);
+graph.addEdge("D", "E", 3);
+graph.addEdge("D", "F", 1);
+graph.addEdge("E", "F", 1);
+
+console.log(graph.Dijkstra("A", "E"));
