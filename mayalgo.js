f5302ec333203854536c39850603720825e97258
MattPelyhes <mattpelyhes@gmail.com>
2021-05-26 17:14:57 -0500

    add candy kids algo solution

--- mayalgo.js
+++ mayalgo.js
@@ -23,3 +23,27 @@ const ipDefang = (str) => {
 }
 
 // console.log(ipDefang("1.1.1.1"))
+
+//Kids with the greatest number of candies
+// Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+// For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
+
+const candyKids = (candies, extraCandies) => {
+  let highestNum = 0;
+  let output = [];
+  for(let el of candies){
+    if(el > highestNum){
+      highestNum = el;
+    }
+  }
+  for(let el of candies){
+    if(el + extraCandies >= highestNum){
+      output.push(true)
+    } else{
+      output.push(false);
+    }
+  }
+  return output;
+}
+
+console.log(candyKids([2,3,5,1,3], 3))
